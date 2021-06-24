5533909653762b5200ad74af0c7e56286386b3d0
= <=>
2021-06-24 10:21:10 +0100

    to-do-list_v2

--- fondamentaux/5.1-événements-state/to-do-list_v2/script.js
+++ fondamentaux/5.1-événements-state/to-do-list_v2/script.js
@@ -9,9 +9,9 @@ class Task extends React.Component {
     class_name += this.props.done ? ' task-success' : ' task-info';
   
     return (
-      <div className={class_name}>
+      <div className={class_name} onClick={this.props.onClickTask}>
         <span>{this.props.value}</span>
-        <i className="close">&times;</i>
+        <i className="close" onClick={this.props.onClickClose}>&times;</i>
       </div>
     )
   }
@@ -24,36 +24,58 @@ class App extends React.Component {
 
     this.state = {
       tasksArray: [
-        {value: 'Tâche 1', done: false},
+        {value: 'Tâche 1', done: true},
+        {value: 'Tâche 2', done: false},
+        {value: 'Tâche 3', done: false}
       ],
       value: ''
     }
-
-   // this.addTask()
-
-    // Simuler l'événement click sur add
-     this.timerID = setInterval(
-      () => this.addTask(),
-      1000
-    );
   }
 
-
-  addTask() {
+  addTask(e) {
     
-    this.state.tasksArray.push({
-      value: "Tâche " +  new Date().getSeconds(),
-      done: false
+    
+     if (addInput.value.length != 0) {
+      this.state.tasksArray.push({
+        value: addInput.value,
+        done: false
+      })
+      
+      
+      this.setState(state => ({
+        tasksArray: state.tasksArray
+      }));
+
+    }
+
+    e.preventDefault()
+  }
+
+  removeTask(i) {
+    this.state.tasksArray.splice(i, 1)
+    this.setState({
+      tasksArray: this.state.tasksArray
+    })
+  }
+
+  markDone(i) {
+    let tasksArray = this.state.tasksArray
+    let task = this.state.tasksArray[i]
+    tasksArray.splice(i, 1)
+    task.done = !task.done 
+    
+    task.done ? tasksArray.push(task) : tasksArray.unshift(task)
+
+
+    this.setState({
+      tasksArray: tasksArray
     })
 
     
+  }
 
-    
-    this.setState(state => ({
-      tasksArray: state.tasksArray
-    }));
-
-   
+  onChangeInput(e) {
+    // this.setState({value: e.target.value})
   }
 
   render() {
@@ -63,6 +85,8 @@ class App extends React.Component {
           key={i}
           value={task.value}
           done={task.done}
+          onClickClose={this.removeTask.bind(this, i)}
+          onClickTask={this.markDone.bind(this, i)}
         />
       )
     })
@@ -75,9 +99,9 @@ class App extends React.Component {
             <form
               id="form-add"
               className="form-horizontal"
-            >
+              onSubmit={this.addTask.bind(this)}>
               <div className="input-group">
-                <input type="text" id="addInput" className="form-control"  placeholder="Description de la tâche..." />
+                <input type="text" id="addInput" className="form-control" onChange={this.onChangeInput.bind(this)} placeholder="Description de la tâche..." />
                 <div className="input-group-btn">
                   <button type="submit" className="btn btn-default">
                     <span className="glyphicon glyphicon-plus-sign"></span>
